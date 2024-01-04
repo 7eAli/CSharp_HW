6918fbdff7180589cfa35939adac9d3e4467d36d
GSY <gle722@yandex.ru>
2024-01-04 14:00:31 +0300

    HW done (probably)

--- 1/HW_1/HW_1/FamilyMember.cs
+++ 1/HW_1/HW_1/FamilyMember.cs
@@ -19,13 +19,16 @@ namespace HW_1
         public FamilyMember mother { get; set; }
         public FamilyMember father { get; set; }
 
+        public FamilyMember betrothed { get; set; }
+
         public FamilyMember() { }
 
-        public FamilyMember(string name, Gender gender, FamilyMember mother, FamilyMember father, params FamilyMember[]? familyMembers)
+        public FamilyMember(string name, Gender gender, FamilyMember mother, FamilyMember father, FamilyMember betrothed, params FamilyMember[]? familyMembers)
         {
             this.name = name;
             this.gender = gender;
             this.children = familyMembers;
+            this.betrothed = betrothed;
             this.mother = mother;
             this.father = father;
         }
@@ -105,6 +108,90 @@ namespace HW_1
             }
             if (children.Count > 0)
                 PrintFamily(children.ToArray());
+        }
+
+        public string CloseRelativesInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            List<FamilyMember> siblings = new List<FamilyMember>();
+            if (this.mother.children != null)
+                foreach (FamilyMember sibling in this.mother.children)
+                {
+                    if (sibling != this)
+                        siblings.Add(sibling);
+                }
+            if (this.father.children != null)
+                foreach (FamilyMember sibling in this.father.children)
+                {
+                    if (sibling != this && !siblings.Contains(sibling))
+                        siblings.Add(sibling);
+                }
+
+            List<FamilyMember> married = new List<FamilyMember>();
+
+            if (this.children != null)
+                foreach (FamilyMember child in this.children)
+                {
+                    if (this.gender == Gender.Male && !married.Contains(child.mother))
+                        married.Add(child.mother);
+                    else if (this.gender == Gender.Female && !married.Contains(child.father))
+                        married.Add(child.father);
+                }
+            sb.Append("Братья и сестры:\n");
+            foreach (FamilyMember sibling in siblings)
+            {
+                if (sibling.gender == Gender.Male) 
+                {
+                    sb.Append($"Брат - {sibling.name}\n");
+                } 
+                else if (sibling.gender == Gender.Female)
+                {
+                    sb.Append($"Сестра - {sibling.name}\n");
+                }
+            }
+            sb.Append("\n");
+            if (this.gender == Gender.Male)
+            {
+                sb.Append("Жены:\n");
+                foreach (FamilyMember betrothed in married)
+                {
+                    if (betrothed == this.betrothed)
+                        sb.Append($"Жена - {betrothed.name}\n");
+                    else
+                        sb.Append($"Бывшая жена - {betrothed.name}\n");
+                }
+            }
+            else if (this.gender == Gender.Female)
+                {
+                    sb.Append("Мужья:\n");
+                    foreach (FamilyMember betrothed in married)
+                    {                        
+                        if (betrothed == this.betrothed)
+                            sb.Append($"Муж - {betrothed.name}\n");
+                        else
+                            sb.Append($"Бывший муж - {betrothed.name}\n");                        
+                    }
+                }
+
+            return sb.ToString();
+        }
+
+        public bool CheckIfSibling(FamilyMember sibling)
+        {
+            bool result = false;
+            if (this.father.children.Contains(sibling)) result = true;
+            if (this.mother.children.Contains(sibling)) result = true;
+            return result;
+        }
+
+        public bool CheckIfBetrothed(FamilyMember betrothed)
+        {
+            bool result = false;
+            foreach (FamilyMember child in betrothed.children)
+            {
+                if (this.children.Contains(child)) result = true;
+            }
+            return result;
         }
     }
 }
--- 1/HW_1/HW_1/Program.cs
+++ 1/HW_1/HW_1/Program.cs
@@ -9,7 +9,12 @@ namespace HW_1
             var grandFather = new FamilyMember() { mother = null, father = null, name = "Дедушка", gender = Gender.Male };
             var grandMother = new FamilyMember() { mother = null, father = null, name = "Бабушка", gender = Gender.Female };
             var father = new FamilyMember() { mother = grandMother, father = grandFather, name = "Папа", gender = Gender.Male };
-            grandFather.children = new FamilyMember[] { father };
+            var uncle1 = new FamilyMember() { mother = grandMother, father = grandFather, name = "Дядя1", gender = Gender.Male };
+            var uncle2 = new FamilyMember() { mother = grandMother, father = grandFather, name = "Дядя2", gender = Gender.Male };
+            var aunt1 = new FamilyMember() { mother = grandMother, father = grandFather, name = "Тетя1", gender = Gender.Female };
+            var aunt2 = new FamilyMember() { mother = grandMother, father = grandFather, name = "Тетя2", gender = Gender.Female };
+            grandFather.children = new FamilyMember[] { father, uncle1, uncle2, aunt1, aunt2 };
+            grandMother.children = new FamilyMember[] { father, uncle1, uncle2, aunt1, aunt2 };
             var mother = new FamilyMember() { mother = null, father = null, name = "Мама", gender = Gender.Female };
             var mother2 = new FamilyMember() { mother = null, father = null, name = "Мама2", gender = Gender.Female };
             var son = new FamilyMember() { mother = mother, father = father, name = "Сын", gender = Gender.Male };
@@ -18,8 +23,11 @@ namespace HW_1
             mother2.children = new FamilyMember[] { son2 };
             father.children = new FamilyMember[] { son, son2 };
 
+            father.betrothed = mother;
+            mother.betrothed = father;
 
-            grandFather.PrintFamily();
+
+            Console.WriteLine(father.CloseRelativesInfo());
         }
     }
 }
