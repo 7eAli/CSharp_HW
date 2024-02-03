3cb5d4f944f803d5b84dffbac664484a4d7717c1
GSY <gle722@yandex.ru>
2024-02-03 08:43:25 +0300

    solved? hw_5

--- 5/Program.cs
+++ 5/Program.cs
@@ -4,7 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calculator calculator = new Calculator();
+            calculator.Result += Calculator_Result;
+
+            int num = 0;
+            bool flag = true;
+            Actions_List();
+
+            while (flag)
+            {
+                Console.WriteLine("Введите число: ");
+                if (int.TryParse(Console.ReadLine(), out num)) 
+                {
+                    Console.WriteLine("Введите действие: ");
+                    flag = Action(Console.ReadLine()!, num, calculator);
+                }
+            }
+        }
+        
+        static void Actions_List()
+        {
+            Console.WriteLine("Действия: ");
+            Console.WriteLine("'+' - сложение");
+            Console.WriteLine("'-' - вычитание");
+            Console.WriteLine("'*' - умножение");
+            Console.WriteLine("'/' - деление");
+            Console.WriteLine("'b' - возврат предыдущего значения");
+            Console.WriteLine("'q' - выход");
+        }
+
+        static bool Action(string action, int num, Calculator calculator)
+        {
+            switch (action)
+            {
+                case "+":
+                    calculator.Add(num);
+                    break;
+                case "-":
+                    calculator.Sub(num);
+                    break;
+                case "*":
+                    calculator.Mult(num);
+                    break;
+                case "/":
+                    calculator.Div(num);
+                    break;
+                case "b":
+                    calculator.Cancel();
+                    break;
+                case "q":
+                    return false;                    
+            }
+            return true;
+        }
+
+        private static void Calculator_Result(object? sender, CalculatorArgs e)
+        {
+            Console.WriteLine(e.answer);
         }
     }
 }
