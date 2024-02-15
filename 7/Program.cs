19bb89141f1c627519067ca18e899a015587ee9a
GSY <gle722@yandex.ru>
2024-02-15 16:01:52 +0300

    Доделал ДЗ-7

--- 7/Program.cs
+++ 7/Program.cs
@@ -11,26 +11,37 @@ namespace _7
     {
         static void Main(string[] args)
         {
-            TestClass testClass = new TestClass(1, "asd", 0.5m, [ 'A', 'B', 'C' ]);
-            
-            Console.WriteLine(testClass.ToString());
+            TestClass testClass = new TestClass(1, "asd", 0.5m, ['A', 'B', 'C']);
+
+
             string s = TypeToString(testClass);
 
-            Console.WriteLine(s);
 
             var obj = StringToObject(s);
-
-            
+            Console.WriteLine(obj.ToString());
         }
 
+        public static Type GetType(string typeName)
+        {
+            var type = Type.GetType(typeName);
+            if (type != null) return type;
+            foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                Console.WriteLine(a.GetType("Program"));
+                type = a.GetType(typeName);
+                if (type != null)
+                    return type;
+            }
+            return null;
+        }
         static string TypeToString(object obj)
         {
             Type t = obj.GetType();
             StringBuilder result = new StringBuilder();
-            result.Append(t.Name);
+            result.Append(t.FullName);
             result.Append('|');
-            foreach (PropertyInfo property in t.GetProperties()) 
-            {                
+            foreach (PropertyInfo property in t.GetProperties())
+            {
                 if (property.GetValue(obj).GetType().IsArray)
                 {
                     result.Append('{');
@@ -52,7 +63,7 @@ namespace _7
             result.Append('|');
             foreach (PropertyInfo property in t.GetProperties())
             {
-                result.Append(property.GetValue(obj).GetType());
+                result.Append(property.GetValue(obj).GetType().FullName);
                 result.Append('=');
                 result.Append(property.Name);
                 result.Append(';');
@@ -63,33 +74,41 @@ namespace _7
 
         static object StringToObject(string s)
         {
-            string[] splitedInfo = s.Split('|');            
-            foreach(string str in splitedInfo)
-            {
-                Console.WriteLine(str);
-            }
-            Assembly assembly = typeof(Assembly).Assembly;                      
-            object? result = assembly.CreateInstance(splitedInfo[0]);
+            string[] splitedInfo = s.Split('|');
+
+            Type type = Type.GetType(splitedInfo[0]);
+            object? result = Activator.CreateInstance(type);
 
             string[] values = splitedInfo[1].Split(";");
             string[] typeInfo = splitedInfo[2].Split(';');
-            
+
             for (int i = 0; i < values.Length - 1; i++)
             {
-                Console.WriteLine(values[i]);                
                 string[] nameAndType = typeInfo[i].Split('=');
-                Console.WriteLine($"{nameAndType[0]} {nameAndType[1]}");
                 Type propertyType = Type.GetType(nameAndType[0]);
                 string propertyName = nameAndType[1];
                 foreach (PropertyInfo property in result.GetType().GetProperties())
                 {
                     if (property.Name == propertyName)
                     {
-                        property.SetValue(result, Convert.ChangeType(values[i], propertyType));
+                        if (property.PropertyType.IsArray)
+                        {
+                            values[i] = values[i].Substring(1, values[i].Length - 2);
+                            string[] arrayValues = values[i].Split(',');
+                            Array array = Array.CreateInstance(propertyType.GetElementType(), arrayValues.Length);
+                            for (int j = 0; j < arrayValues.Length; j++)
+                            {
+                                array.SetValue(Convert.ChangeType(arrayValues[j], array.GetType().GetElementType()), j);
+                                Console.WriteLine(array.GetValue(j));
+                            }
+                            property.SetValue(result, Convert.ChangeType(array, propertyType));
+                        }
+                        else
+                            property.SetValue(result, Convert.ChangeType(values[i], propertyType));
                     }
                 }
             }
-                       
+
             return result;
         }
     }
